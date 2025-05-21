64bf96c7296366e04a8f353ab38f87f6bdc4abf5
Pawel Wolak <pwolak@dropbox.com>
2025-05-21 11:21:53 +0200

    Initial version

---
+++ src/App.tsx
@@ -0,0 +1,94 @@
+import { useEffect, useMemo, useState } from "react";
+import logo from "/logo.png";
+
+interface Result {
+  id: string;
+  name: string;
+  score: number;
+}
+
+function Leaderboard({
+  data,
+  offset,
+}: {
+  data: (Result | null)[];
+  offset: number;
+}) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th className="center pos">Pos</th>
+          <th className="left player">Player</th>
+          <th className="center points">Points</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.slice(offset, offset + 10).map((result, index) => (
+          <tr key={index}>
+            <td className="center pos">{index + offset + 1}</td>
+            <td className="left player">{result?.name}</td>
+            <td className="center points">{result?.score}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+function App() {
+  const [results, setResults] = useState<Result[]>([]);
+  const [updated, setUpdated] = useState(new Date());
+
+  const paddedResults = useMemo(
+    () => results.concat(Array(20).fill(null)).slice(0, 20),
+    [results]
+  );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://sheets.googleapis.com/v4/spreadsheets/${import.meta.env.VITE_SHEET_ID}/values/Results?key=${import.meta.env.VITE_API_KEY}`
+      );
+      const { values } = await response.json();
+      setResults(
+        (values as [string, string, string][])
+          .slice(1)
+          .map(([id, name, score]) => ({
+            id,
+            name,
+            score: parseInt(score, 10),
+          }))
+      );
+      setUpdated(new Date());
+    };
+
+    fetchData();
+    const interval = setInterval(fetchData, 60000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <header className="header">
+        <h1>
+          <img src={logo} alt="Dropbox Dev Prix" className="logo" />
+        </h1>
+      </header>
+      <main>
+        <div className="column">
+          <Leaderboard data={paddedResults} offset={0} />
+          <p>
+            Ranking is based on points and time it took to complete the test.
+          </p>
+          <p>Last updated: {updated.toLocaleString()}</p>
+        </div>
+        <div className="column">
+          <Leaderboard data={paddedResults} offset={10} />
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
